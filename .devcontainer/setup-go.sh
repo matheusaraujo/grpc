521d617181746b2696dd51b82e7e5c0ca223a6b1
Matheus Araujo <matheus.saraujo@gmail.com>
2024-11-08 15:29:23 +0000

    feat: golang

--- .devcontainer/setup-go.sh
+++ .devcontainer/setup-go.sh
@@ -9,6 +9,6 @@ PROTOC_GEN_GO_GRPC_VERSION=1.5.1
 curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/protoc-$PROTOC_VERSION-linux-x86_64.zip
 unzip protoc-$PROTOC_VERSION-linux-x86_64.zip -d $HOME/.local
 rm protoc-$PROTOC_VERSION-linux-x86_64.zip
-export PATH="$PATH:$HOME/.local/bin"
+echo "export PATH=\$PATH:$HOME/.local/bin" >> /etc/profile
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v$PROTOC_GEN_GO_VERSION
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v$PROTOC_GEN_GO_GRPC_VERSION
--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # grpc
 
+a grpc hello world implementation
+
+### running go server
+
 ```bash
 $ go run server.go
 ```
 
+### running go client
+
 ```bash
 $ go run client.go
 ```
+
+### todo
+- [ ] python
+- [ ] node
+- [ ] c++
